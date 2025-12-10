99f814b2cdfe65834c387fd973df5f6ac4231738
Sukh-Ochir927 <sukhochirbatjargal27@gmail.com>
2025-12-10 17:19:04 +0800

    Update todo list design and functionality

--- script.js
+++ script.js
@@ -6,15 +6,64 @@ const taskSectionContainer = document.querySelector("#task_section_container");
 
 const taskArr = [];
 
-let taskId = 1;
+let taskObjID = 1;
 
 const add = () => {
-  const textTask = input.value;
-
+  const taskText = input.value;
   const taskObj = {
-    id: taskId,
-    text: textTask,
-    isComplate: false,
+    text: taskText,
+    isCompleted: false,
+    objID: taskObjID,
   };
-  taskArr.push(textTask);
+
+  taskArr.push(taskObj);
+
+  taskObjID++;
+
+  taskView();
+  clearInput();
+  console.log(taskArr);
 };
+
+input.addEventListener("keydown", (e) => {
+  if (e.key === "Enter" && input.value.trim() !== "") {
+    add();
+  }
+});
+
+const taskView = () => {
+  let taskDisplay = "";
+  taskArr.forEach((task) => {
+    const task$ = ` 
+        <div class="task">
+          <input type="checkbox" ${
+            task.isCompleted && "checked"
+          } onchange="isChecked(${task.objID}, this )"/>
+          <p>${task.text}</p>
+          <button>Delete</button>
+        </div>`;
+
+    taskDisplay += task$;
+  });
+
+  taskSectionContainer.innerHTML = taskDisplay;
+};
+
+const isChecked = (id, checkbox) => {
+  // console.log(id);
+  console.log(checkbox);
+
+  if ((checkbox.checked = true)) {
+    return console.log("Yes it is True that checkbox Checked");
+  } else {
+    console.log(task.isCompleted);
+  }
+};
+
+const clearInput = () => {
+  input.value = "";
+};
+
+// Odoo checkbox checkleh uyd completedruu shiljih bolon checkbox checklegdeegui bol active tab-d haragddag baidlaar hiine
+
+// Teghiin tuld #1 checkbox checkleh uyd true utgiig ugdug boliy.
